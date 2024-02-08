b17130861b64399dc48f0e5f9d6873d5ca040f65
hiimlyh <mylovedarts@naver.com>
2024-02-09 01:10:27 +0900

    test

---
+++ src/main/java/com/chloe/ChloeApplication.java
@@ -0,0 +1,13 @@
+package com.chloe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChloeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChloeApplication.class, args);
+	}
+
+}
